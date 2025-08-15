47b8dd43acddb692ff238bcc77f7c63702fec3af
NguyenNhuDuyHung <duyhung03112004@gmail.com>
2025-08-15 21:26:04 +0700

    base jwt

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserResolver } from './user.resolver';
+import { Module } from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserResolver } from './user.resolver'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
-  providers: [UserResolver, UserService],
+  imports: [],
+  providers: [UserResolver, UserService, PrismaService],
 })
 export class UserModule {}
