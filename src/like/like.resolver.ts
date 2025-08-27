f8de965ddab2e9a43a4e57829f5a88561676ed40
NguyenNhuDuyHung <duyhung03112004@gmail.com>
2025-08-27 14:04:08 +0700

    feat: add like and unlike functionality; implement post like count and user liked post queries

--- src/like/like.resolver.ts
+++ src/like/like.resolver.ts
@@ -1,8 +1,45 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { LikeService } from './like.service'
 import { Like } from './entities/like.entity'
+import { UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard'
 
 @Resolver(() => Like)
 export class LikeResolver {
   constructor(private readonly likeService: LikeService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Boolean)
+  async likePost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number,
+  ) {
+    const userId = context.req.user.id
+    return await this.likeService.likePost({ postId, userId })
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Boolean)
+  async unlikePost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number,
+  ) {
+    const userId = context.req.user.id
+    return await this.likeService.unlikePost({ postId, userId })
+  }
+
+  @Query(() => Int)
+  async postLikeCount(@Args('postId', { type: () => Int }) postId: number) {
+    return await this.likeService.getPostLikesCount(postId)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Query(() => Boolean)
+  async userLikedPost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number,
+  ) {
+    const userId = context.req.user.id
+    return await this.likeService.userLikedPost({ postId, userId })
+  }
 }
